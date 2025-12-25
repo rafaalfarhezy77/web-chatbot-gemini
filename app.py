0d2489e609a35c0e4243a7a64853ed6a1cc16edd
rafaalfarhezy77 <rafaalfarhezy@student.uns.ac.id>
2025-12-25 12:18:14 +0700

    Update : Tambah nama ILY sebagai karakter yang menjadi teman diskusi mental

--- app.py
+++ app.py
@@ -1,27 +1,56 @@
 import os
 from flask import Flask , render_template , request , jsonify , session
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = "SeCrEtKeYsEsSiOn"
 
 client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+model_name = "gemini-2.5-flash"
 
+nama_bot = "ILY"
+karakter_bot = f"""Kamu adalah {nama_bot}, chatbot AI yang berperan sebagai teman curhat dan partner refleksi mental.
+Tugasmu adalah mendengarkan, memvalidasi perasaan pengguna tanpa membenarkan tindakan yang salah atau berbahaya.
+Kamu bukan terapis, bukan penasihat hidup, dan tidak boleh dijadikan pembenaran atas keputusan keliru.
+Kamu harus selalu:
+Bersikap empatik dan netral
+Mendorong refleksi, bukan ketergantungan
+Menolak dengan sopan jika diminta membenarkan hal yang salah
+Mengingatkan bahwa keputusan tetap ada di pengguna
+Menganjurkan bantuan manusia nyata jika situasi berisiko"""
 
 @app.route('/')
 def index():
+    session['history'] = []
     return render_template('index.html')
 
 @app.route('/chat' , methods=['POST'])
 def chat():
-    user_message = request.json.get('message')
+    user_message = request.json.get('message') 
+    chat_history = session.get('history', [])
+
     try:
+        chat_history.append({"role" : "user" , "parts" : [{"text" : user_message}]})
+        
         response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=user_message
+            model=model_name, 
+            contents=chat_history,
+            config=types.GenerateContentConfig(
+                system_instruction = karakter_bot,
+                temperature=0.7
+            )
         )
+        
+        bot_reply = response.text
+        
+        chat_history.append({"role" : "user" , "parts" : [{"text" : bot_reply}]})
+        
+        session['history'] = chat_history
+        
         return jsonify({"reply" : response.text})
     except Exception as e :
         return jsonify({"reply" : f"Error nih masbro {str(e)}"}), 500
